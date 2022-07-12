15f9e3d663fb94041135088cbda81be81af7e099
biamesquitap <beatrizmesquitta@yahoo.com>
2022-07-12 17:44:17 -0300

    Boa noite

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
 import { Header } from "./components/header";
 import { Post } from "./Post";
+
+import styles from "./App.module.css"
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 
 export function App () {
  return (
    <div>
     <Header />
-    <Post
-      author='Bia M'
-      content='1, 2,3 indiozinhos'
-    />
-    <Post
-      author='B. M.'
-      content='pastel de churros'
-    />
-
+    
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post
+          author='Bia M'
+          content='1, 2,3 indiozinhos'
+        />
+        <Post
+          author='B. M.'
+          content='pastel de churros'
+        />
+      </main>
+    </div>
    </div>
   )
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,26 @@
+import styles from "./Sidebar.module.css";
+
+export function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img 
+      className={styles.cover}
+      src="https://images.unsplash.com/photo-1489875347897-49f64b51c1f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" 
+      />
+
+      <div className={styles.profile}>
+        <img 
+        className={styles.avatar}
+        src="https://github.com/biamesquitap.png"
+        />
+        <strong> Beatriz Mesquita </strong> 
+        <span> Web developer </span>
+      </div>
+
+      <footer>
+        <a href="#"> Editar seu perfil </a>
+      </footer>
+
+    </aside>
+  );
+}
