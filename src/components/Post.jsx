6e9f4f978d73720fb23cc24af776c40265492ea9
Beatriz Mesquita <beatrizmesquitta@yahoo.com>
2022-07-17 00:38:15 -0300

    sos

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,7 +7,7 @@ export function Post() {
         <div className={styles.author}>
           <img
           className={styles.avatar}
-          src="https://github.com/biamesquitap.png" 
+          src="https://github.com/biamesquitap.png"
           />
           <div>
             <strong className={styles.authorInfo}> Beatriz Mesquita </strong>
@@ -22,11 +22,20 @@ export function Post() {
         <p>Fala galeraa 👋</p>
         <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
         <p> 👉 {'   '} <a href=""> jane.design/doctorcare </a> </p>
-        <p> 
-          <a href=""> #novoprojeto </a> {' '} 
-          <a href=""> #nlw </a>  {' '} 
+        <p>
+          <a href=""> #novoprojeto </a> {' '}
+          <a href=""> #nlw </a>  {' '}
           <a href=""> #rocketseat </a> </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentario"/>
+      </form>
+
+      <footer>
+        <button type="submit"> Publicar </button>
+      </footer>
     </article>
   )
 }
